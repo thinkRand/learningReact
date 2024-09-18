35458acfbf27527e59329ccb8a9814ec294b8a7c
Abel Granados <leonivera24c@gmail.com>
2024-09-18 00:13:39 -0400

    next fundamentals

---
+++ next/ejemplo/nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        <div>
+          {children}
+        </div>
+        </body>
+    </html>
+  );
+}
