b0cae9f77f45b8686112d180f65049f7340b5312
Abel Granados <leonivera24c@gmail.com>
2024-09-25 18:33:58 -0400

    chapter 14

--- next/ejemplo/nextjs-dashboard/app/lib/data.ts
+++ next/ejemplo/nextjs-dashboard/app/lib/data.ts
@@ -151,13 +151,14 @@ export async function fetchInvoiceById(id: string) {
       FROM invoices
       WHERE invoices.id = ${id};
     `;
-
+    
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
 
+    
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
